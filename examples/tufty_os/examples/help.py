ff002522a44b4f22bee04f12d12c59ac3731978d
thirdr <ryan@pimoroni.com>
2025-06-11 13:51:30 +0100

    Examples: adding help.py

--- examples/tufty_os/examples/help.py
+++ examples/tufty_os/examples/help.py
@@ -2,16 +2,15 @@
 # NAME Help
 # DESC How to use your Tufty 2350.
 
-import tufty2350
-from tufty2350 import WIDTH
 from picovector import ANTIALIAS_BEST, PicoVector, Polygon, Transform
 
-TEXT_SIZE = 0.45
+import tufty2350
+from tufty2350 import WIDTH
+
 LINE_HEIGHT = 20
 
 display = tufty2350.Tufty2350()
 display.led(128)
-display.set_thickness(2)
 
 # Pico Vector
 vector = PicoVector(display.display)
@@ -19,47 +18,48 @@ vector.set_antialiasing(ANTIALIAS_BEST)
 t = Transform()
 
 TITLE_BAR = Polygon()
-TITLE_BAR.rectangle(2, 2, 260, 16, (8, 8, 8, 8))
-TITLE_BAR.circle(253, 10, 4)
+TITLE_BAR.rectangle(2, 2, WIDTH - 4, 16, (8, 8, 8, 8))
+TITLE_BAR.circle(WIDTH - 10, 10, 4)
 
 TEXT_BOX = Polygon()
-TEXT_BOX.rectangle(2, 30, 260, 125, (8, 8, 8, 8))
+TEXT_BOX.rectangle(2, 65, WIDTH - 4, 130, (8, 8, 8, 8))
 
-# Clear to white
-display.set_pen(15)
-display.clear()
+# Colours
+BACKGROUND = display.create_pen(158, 188, 138)
+FOREGROUND = display.create_pen(210, 208, 160)
+HIGHLIGHT = display.create_pen(115, 148, 107)
 
-display.set_font("bitmap8")
-display.set_pen(0)
-vector.draw(TITLE_BAR)
-display.set_pen(15)
-display.text("TuftyOS", 7, 6, WIDTH, 1.0)
-display.text("help", WIDTH - 40, 6, WIDTH, 1)
 
-display.set_pen(2)
-vector.draw(TEXT_BOX)
-
-display.set_pen(1)
-TEXT_SIZE = 2.0
-y = 35 + int(LINE_HEIGHT / 2)
-x = 5
-
-display.set_font("bitmap8")
-display.text("Up/Down - Move up and down", x, y, WIDTH, TEXT_SIZE)
-y += LINE_HEIGHT
-display.text("a - Move left", x, y, WIDTH, TEXT_SIZE)
-y += LINE_HEIGHT
-display.text("b - Launch selected app", x, y, WIDTH, TEXT_SIZE)
-y += LINE_HEIGHT
-display.text("c - Move right", x, y, WIDTH, TEXT_SIZE)
-y += LINE_HEIGHT
-display.text("a & c - Exit app", x, y, WIDTH, TEXT_SIZE)
-y += LINE_HEIGHT
-
-display.update()
-
-# Call halt in a loop, on battery this switches off power.
-# On USB, the app will exit when A+C is pressed because the launcher picks that up.
 while True:
-    display.keepalive()
-    display.halt()
+    # Clear to white
+    display.set_pen(BACKGROUND)
+    display.clear()
+
+    display.set_font("bitmap8")
+    display.set_pen(FOREGROUND)
+    vector.draw(TITLE_BAR)
+    display.set_pen(HIGHLIGHT)
+    display.text("TuftyOS", 7, 6, WIDTH, 1.0)
+    display.text("help", WIDTH - 40, 6, WIDTH, 1)
+
+    display.set_pen(FOREGROUND)
+    vector.draw(TEXT_BOX)
+
+    display.set_pen(HIGHLIGHT)
+    TEXT_SIZE = 2.0
+    y = 72 + int(LINE_HEIGHT / 2)
+    x = 7
+
+    display.set_font("bitmap8")
+    display.text("Up/Down - Move up and down", x, y, WIDTH, TEXT_SIZE)
+    y += LINE_HEIGHT
+    display.text("a - Move left", x, y, WIDTH, TEXT_SIZE)
+    y += LINE_HEIGHT
+    display.text("b - Launch selected app", x, y, WIDTH, TEXT_SIZE)
+    y += LINE_HEIGHT
+    display.text("c - Move right", x, y, WIDTH, TEXT_SIZE)
+    y += LINE_HEIGHT
+    display.text("a & c - Exit app", x, y, WIDTH, TEXT_SIZE)
+    y += LINE_HEIGHT
+
+    display.update()
