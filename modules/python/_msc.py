f2ee69311b490d173c74aa955950104834bbbef8
Phil Howard <github@gadgetoid.com>
2025-09-16 14:36:45 +0100

    MSC: Fix msc notification.

--- modules/python/_msc.py
+++ modules/python/_msc.py
@@ -1,5 +1,5 @@
-from picographics import PicoGraphics, DISPLAY_BADGER_2350
-display = PicoGraphics(DISPLAY_BADGER_2350)
+from picographics import PicoGraphics, DISPLAY_EXPLORER
+display = PicoGraphics(DISPLAY_EXPLORER, rotate=180)
 display.set_pen(0)
 display.clear()
 display.set_pen(0x22)
