48107175c1bd00f0864558613a4637a5af2ec42a
thirdr <ryan@pimoroni.com>
2025-09-17 10:24:23 +0100

    Build: Adding hardware test

--- modules/common/boot.py
+++ modules/common/boot.py
@@ -1,3 +1,9 @@
 import cppmem
 # Switch C++ memory allocations to use MicroPython's heap
 cppmem.set_mode(cppmem.MICROPYTHON)
+
+try:
+    with open("state/hardware_test.txt", "r"):
+        import hardware_test   # noqa F401
+except OSError:
+    pass
