4b20cff30b059b44b08ff47609682365812e4f8f
Phil Howard <github@gadgetoid.com>
2026-02-05 17:58:19 +0000

    badgeware/rtc: Add more alarm functions.

--- modules/common/badgeware/rtc.py
+++ modules/common/badgeware/rtc.py
@@ -5,6 +5,7 @@ import pcf85063a
 import datetime
 import gc
 import builtins
+import board
 
 
 class RTC:
@@ -42,6 +43,15 @@ class RTC:
         except IndexError:
             return None
 
+    def set_timer(self, ticks, enable_interrupt=True):
+        self._rtc.enable_timer_interrupt(enable_interrupt)
+        self._rtc.set_timer(ticks)
+
+    def timer_elapsed(self):
+        result = self._rtc.read_timer_flag()
+        self._rtc.clear_timer_flag()
+        return result
+
     def set_alarm(self, hours=0, minutes=0, seconds=0):
         self._rtc.clear_alarm_flag()
         self._rtc.clear_timer_flag()
@@ -56,6 +66,15 @@ class RTC:
         dt += datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
 
         self._rtc.set_alarm(dt.second, dt.minute, dt.hour)
+
+    def clear_alarm(self):
+        self._rtc.enable_alarm_interrupt(False)
+        self._rtc.clear_alarm_flag()
+        self._rtc.clear_timer_flag()
+        self._rtc.unset_alarm()
+
+    def alarm_status(self):
+        return board.RTC_ALARM.value() == 0
 
 
 builtins.rtc = RTC()
