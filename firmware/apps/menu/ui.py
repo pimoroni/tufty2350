d17cdf9834737a62d2f2080254a911ea32bf54d4
thirdr <ryan@pimoroni.com>
2026-02-25 11:11:22 +0000

    apps: removed dummy version number from menu

--- firmware/apps/menu/ui.py
+++ firmware/apps/menu/ui.py
@@ -87,7 +87,7 @@ def draw_terminal():
 def draw_header():
     # create animated header text
     dots = "." * int(math.sin(badge.ticks / 250) * 2 + 2)
-    label = f"BadgeOS v4.03{dots}"
+    label = f"BadgeOS{dots}"
     pos = (5, 2)
 
     # draw the OS title
